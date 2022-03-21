d25c37478682a641d4d409023c18d00ab96ce61a
Lyubarskikh Kirill <lyubarskikh@capitalpolis.ru>
2022-03-21 15:56:27 +0300

    Init commit

---
+++ src/main/java/ru/kirill98/make3dModels/Make3dModelsApplication.java
@@ -0,0 +1,13 @@
+package ru.kirill98.make3dModels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Make3dModelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Make3dModelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kirill98/make3dModels/Make3dModelsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kirill98.make3dModels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Make3dModelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
