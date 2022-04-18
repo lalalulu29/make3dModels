f8874ad880a02f765cee24e937207af81381f8bb
Kirill Lyubarskikh <vil.rik@mail.ru>
2022-04-18 16:41:27 +0300

    Add users entities

--- src/main/java/ru/kirill98/make3dModels/entities/Users.java
+++ src/main/java/ru/kirill98/make3dModels/entities/Users.java
@@ -3,6 +3,7 @@ package ru.kirill98.make3dModels.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.sql.Date;
@@ -12,15 +13,20 @@ import java.sql.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Users {
+    @Column("Username")
     private String username;
+    @Column("Password")
     private String password;
 
+    @Column("Firstname")
     private String firstName;
+    @Column("Lastname")
     private String lastName;
+    @Column("Email")
     private String email;
 
+    @Column("Birth_date")
     private Date birthDate;
+    @Column("Reg_date")
     private Date regDate;
-
-
 }
