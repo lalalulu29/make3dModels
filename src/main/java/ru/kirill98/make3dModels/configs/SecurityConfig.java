4a63354d8cfb59fff38241c1542244a58c164d85
Kirill Lyubarskikh <vil.rik@mail.ru>
2022-04-25 12:27:58 +0300

    - Added a field with information about the last page update

--- src/main/java/ru/kirill98/make3dModels/configs/SecurityConfig.java
+++ src/main/java/ru/kirill98/make3dModels/configs/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/log/**").hasRole("ADMIN")
                 .and()
                 .formLogin()
+//				.httpBasic()
 				.and()
                 .logout().logoutSuccessUrl("/")
                 .and();
